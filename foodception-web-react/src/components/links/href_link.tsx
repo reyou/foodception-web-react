eb851cc82c3b23f5e5fb394781147782fadc43da
teoman shipahi <aytekoz@yahoo.com>
2024-10-05 21:26:09 -0500

    event.button

--- foodception-web-react/src/components/links/href_link.tsx
+++ foodception-web-react/src/components/links/href_link.tsx
@@ -15,23 +15,34 @@ const FoodceptionHrefLink: React.FC<FoodceptionHrefLinkProps> = ({
     event: React.MouseEvent<HTMLAnchorElement>,
     url: string
   ) => {
-    event.preventDefault();
-    ParentWindowUtils.postMessage({ type: 'redirect', url: url });
+    // Only prevent default behavior for left-clicks (event.button === 0)
+    if (event.button === 0) {
+      event.preventDefault();
+      ParentWindowUtils.postMessage({ type: 'redirect', url: url });
+    }
   };
 
   if (FrontEndUtils.isInsideIframe()) {
     return (
       <a
+        data-guid='409a1aaf-a90c-4997-8a19-a5def439b9f2'
         className='link-button'
         href={url}
         onClick={(event) => handleLinkClick(event, url)}
+        target='_blank'
+        rel='noopener noreferrer'
       >
         {children}
       </a>
     );
   } else {
     return (
-      <a className='link-button' href={url}>
+      <a
+        data-guid='8c3ab256-8ccf-4bd4-adab-60f7046dc288'
+        className='link-button'
+        href={url}
+        rel='noopener noreferrer'
+      >
         {children}
       </a>
     );
