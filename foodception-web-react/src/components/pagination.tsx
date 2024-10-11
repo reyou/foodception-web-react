f24a46bf6a218d5eaf0c926729d0787fce86e8c4
teoman shipahi <aytekoz@yahoo.com>
2024-10-10 22:38:50 -0500

    pagination updated

--- foodception-web-react/src/components/pagination.tsx
+++ foodception-web-react/src/components/pagination.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Pagination } from 'react-bootstrap';
 import { FrontEndUtils } from '../utils/FrontEndUtils';
 import ParentWindowUtils from '../utils/ParentWindowUtils';
 
@@ -6,7 +7,7 @@ interface PaginationProps {
   currentPage: number;
 }
 
-const Pagination: React.FC<PaginationProps> = ({ currentPage }) => {
+const PaginationComponent: React.FC<PaginationProps> = ({ currentPage }) => {
   // Helper function to construct a URL with the page parameter
   const constructUrlWithPage = (page: number) => {
     const url = new URL(window.location.href);
@@ -16,7 +17,7 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage }) => {
   };
 
   const handlePageChange = (
-    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    event: React.MouseEvent<HTMLElement, MouseEvent>,
     page: number
   ) => {
     const urlWithPage = constructUrlWithPage(page);
@@ -27,51 +28,34 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage }) => {
   };
 
   return (
-    <nav aria-label='Page navigation'>
-      <ul className='pagination justify-content-center'>
-        {/* First Page Link */}
-        <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
-          <a
-            className='page-link'
-            href={constructUrlWithPage(1)}
-            onClick={(e) => {
-              handlePageChange(e, 1);
-            }}
-            aria-disabled={currentPage === 1}
-          >
-            First
-          </a>
-        </li>
+    <Pagination className='justify-content-center text-secondary'>
+      {/* First Page Link */}
+      <Pagination.First
+        onClick={(e) => handlePageChange(e, 1)}
+        disabled={currentPage === 1}
+        href={constructUrlWithPage(1)}
+      >
+        First
+      </Pagination.First>
 
-        {/* Previous Page Link */}
-        <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
-          <a
-            className='page-link'
-            href={constructUrlWithPage(currentPage - 1)}
-            onClick={(e) => {
-              handlePageChange(e, currentPage - 1);
-            }}
-            aria-disabled={currentPage === 1}
-          >
-            Previous
-          </a>
-        </li>
+      {/* Previous Page Link */}
+      <Pagination.Prev
+        onClick={(e) => handlePageChange(e, currentPage - 1)}
+        disabled={currentPage === 1}
+        href={constructUrlWithPage(currentPage - 1)}
+      >
+        Previous
+      </Pagination.Prev>
 
-        {/* Next Page Link */}
-        <li className='page-item'>
-          <a
-            className='page-link'
-            href={constructUrlWithPage(currentPage + 1)}
-            onClick={(e) => {
-              handlePageChange(e, currentPage + 1); // Call the callback function
-            }}
-          >
-            Next
-          </a>
-        </li>
-      </ul>
-    </nav>
+      {/* Next Page Link */}
+      <Pagination.Next
+        onClick={(e) => handlePageChange(e, currentPage + 1)}
+        href={constructUrlWithPage(currentPage + 1)}
+      >
+        Next
+      </Pagination.Next>
+    </Pagination>
   );
 };
 
-export default Pagination;
+export default PaginationComponent;
