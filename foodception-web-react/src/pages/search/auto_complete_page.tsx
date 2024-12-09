abe95068b7cd98a809e125854da6adc9989a61de
teoman shipahi <aytekoz@yahoo.com>
2024-12-08 20:09:57 -0600

    SearchAutoComplete

--- foodception-web-react/src/pages/search/auto_complete_page.tsx
+++ foodception-web-react/src/pages/search/auto_complete_page.tsx
@@ -2,11 +2,8 @@ import { useEffect } from 'react';
 import { useLayout } from '../../contexts/layout-context';
 import { Col, Row } from 'react-bootstrap';
 import SearchAutoComplete from '../../components/search_auto_complete';
-import { useNavigate } from 'react-router-dom';
-import { FrontEndUtils } from '../../utils/FrontEndUtils';
 
 export default function SearchAutoCompletePage() {
-  const navigate = useNavigate();
   const { setShowBreadcrumb, setShowHorizontalRule } = useLayout();
   useEffect(() => {
     setShowBreadcrumb(false);
@@ -17,18 +14,15 @@ export default function SearchAutoCompletePage() {
     };
   }, [setShowBreadcrumb, setShowHorizontalRule]);
 
-  const handleSearch = (term: string) => {
-    FrontEndUtils.redirect(`/search?query=${term}`, navigate);
-  };
-
   return (
     <div>
       <Row className='justify-content-center'>
         <Col xs={12} md={6} lg={4} xl={3}>
           <SearchAutoComplete
             initialSearchTerm={''}
-            onSearch={handleSearch}
+            onSearch={() => {}}
             apiEndpoint='/search/autocomplete'
+            baseUrl='/search'
           />
         </Col>
       </Row>
