0a626f7e97445b630213becd112ded78b6e68cfe
teoman shipahi <aytekoz@yahoo.com>
2025-01-08 19:34:24 -0600

    feat: add pagination to category recipes

--- foodception-web-react/src/pages/recipeCategoryRecipes.page.tsx
+++ foodception-web-react/src/pages/recipeCategoryRecipes.page.tsx
@@ -7,13 +7,16 @@ import { Container, Row, Col } from 'react-bootstrap';
 import LoadingPanel from '../components/loading_panel';
 import HeaderLayout from '../components/header/headerLayout';
 import { FrontEndUtils } from '../utils/FrontEndUtils';
+import Pagination from '../components/pagination';
+import { useQuery } from '../hooks/useQuery';
 
-interface RecipeCategoryRecipesPageProps {}
-
-const RecipeCategoryRecipesPage: React.FC<RecipeCategoryRecipesPageProps> = () => {
+const RecipeCategoryRecipesPage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
-  const { data, loading, error } = useFetch(`/recipe-categories/${id}/recipes`);
-
+  const query = useQuery();
+  const page = parseInt(query.get('page') || '1');
+  const limit = parseInt(query.get('limit') || '20');
+  const skip = (page - 1) * limit;
+  const { data, loading, error } = useFetch(`/recipe-categories/${id}/recipes?skip=${skip}&limit=${limit}`);
   if (loading) {
     return <LoadingPanel visible={loading}></LoadingPanel>;
   }
@@ -41,7 +44,14 @@ const RecipeCategoryRecipesPage: React.FC<RecipeCategoryRecipesPageProps> = () =
       <Container fluid>
         <Row>
           <Col>
+            <div className='mb-4 mt-4 text-center'>
+              <h4 className='text-muted'>
+                {data.totalCount}{' '}
+                {data.totalCount === 1 ? 'Recipe' : 'Recipes'} Found
+              </h4>
+            </div>
             <RecipeList recipes={data.recipes} />
+            <Pagination currentPage={page} />
           </Col>
         </Row>
       </Container>
