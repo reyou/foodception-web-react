62fe52e09d64086bda484ec910fafb528c0631b8
teoman shipahi <aytekoz@yahoo.com>
2025-02-26 23:29:54 -0600

    feat: fix css classes

--- foodception-web-react/src/pages/user/auth/google/RedirectingToGoogle.page.tsx
+++ foodception-web-react/src/pages/user/auth/google/RedirectingToGoogle.page.tsx
@@ -3,22 +3,24 @@ import './RedirectingToGoogle.page.styles.css';
 
 const RedirectingToGoogle: React.FC = () => {
   return (
-    <div className="redirect-container">
-      <div className="redirect-content">
-        <h3>Redirecting to Google</h3>
-        <p>Please wait while we redirect you to Google to complete the sign-in process. You will be redirected
-          automatically in a few seconds.</p>
+    <div className="rtg-container">
+      <div className="redirect-container">
+        <div className="redirect-content">
+          <h3>Redirecting to Google</h3>
+          <p>Please wait while we redirect you to Google to complete the sign-in process. You will be redirected
+            automatically in a few seconds.</p>
 
-        <div className="google-logo">
-          <span className="g-blue">G</span>
-          <span className="g-red">o</span>
-          <span className="g-yellow">o</span>
-          <span className="g-blue">g</span>
-          <span className="g-green">l</span>
-          <span className="g-red">e</span>
+          <div className="google-logo">
+            <span className="g-blue">G</span>
+            <span className="g-red">o</span>
+            <span className="g-yellow">o</span>
+            <span className="g-blue">g</span>
+            <span className="g-green">l</span>
+            <span className="g-red">e</span>
+          </div>
+
+          <div className="loader"></div>
         </div>
-
-        <div className="loader"></div>
       </div>
     </div>
   );
