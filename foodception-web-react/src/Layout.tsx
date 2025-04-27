17306c305856e1d236b12d8cbbceac42501bc7d3
teoman shipahi <aytekoz@yahoo.com>
2025-04-27 14:31:59 -0500

    feat: added search

--- foodception-web-react/src/Layout.tsx
+++ foodception-web-react/src/Layout.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
 
 import DynamicBreadcrumbs from './components/core/foodception_breadcrumbs';
 import { useLayout } from './contexts/layout-context';
 import FoodceptionNavbar from './components/navbar/Navbar';
+import SearchAutoComplete from './components/search_auto_complete';
 
 const Layout: React.FC = () => {
   const { showNavigation, showBreadcrumb, showHorizontalRule } = useLayout();
   return (
     <>
       {showNavigation && <FoodceptionNavbar />}
+      <Container fluid>
+        <Row className="justify-content-center g-0 mb-3">
+          <Col xs={12} sm={10} md={8} lg={6} xl={5}>
+            <SearchAutoComplete
+              initialSearchTerm={''}
+              onSearch={() => { }}
+              apiEndpoint='/search/autocomplete'
+              baseUrl='/search'
+            />
+          </Col>
+        </Row>
+      </Container>
       {showBreadcrumb && <DynamicBreadcrumbs />}
       <Outlet />
       {showHorizontalRule && <hr />}
